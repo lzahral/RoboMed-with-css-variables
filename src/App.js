d3d29d6e3191e20d0303c7893eb1aabebcab2c93
Zahra Farzane <farzanehzf81@gmail.com>
2022-07-22 11:56:11 +0430

    robomed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import NavbarComp from './components/NavbarComp';
-import MainPart from './components/MainPart';
-import Facilities from './components/Facilities/Facilities';
-import Slider from './components/slider/Slider';
-import Team from './components/teamDoctor/Team';
-import Subscribe from './components/Subscribe';
-import Footer from './components/Footer';
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import NavbarComp from "./components/NavbarComp";
+import MainPart from "./components/MainPart";
+import Facilities from "./components/Facilities/Facilities";
+import Slider from "./components/slider/Slider";
+import Team from "./components/teamDoctor/Team";
+import Subscribe from "./components/Subscribe";
+import Footer from "./components/Footer";
 
 const App = () => {
-  const [Theme, setTheme] = useState(false);
-  return (
-    <div  id="theme" className={Theme ? "light p-4" : "dark p-4"}>
-      <Container>
-        <NavbarComp theme={Theme} setTheme={setTheme}/>
-        <MainPart theme={Theme}/>
-        <Facilities/>
-        <Slider/>
-        <Team/>
-        <Subscribe/>
-        <Footer/>
-      </Container>
-    </div>
-  );
+   const [theme, setTheme] = useState(false);
+   return (
+      <div id="theme" className={theme ? "light p-4" : "dark p-4"}>
+         <Container>
+            <NavbarComp theme={theme} setTheme={setTheme} />
+            <MainPart theme={theme} />
+            <Facilities />
+            <Slider />
+            <Team />
+            <Subscribe />
+            <Footer />
+         </Container>
+      </div>
+   );
 };
 
 export default App;
--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -9,37 +9,37 @@ import { IoSunnyOutline, IoMoonOutline } from "react-icons/io5";
 
 
 const NavbarComp = ({ theme, setTheme }) => {
-  return (
-    <Navbar collapseOnSelect expand="lg" variant={!theme ? "dark" : "light"}>
-      <Container className='me-auto'>
-        <Navbar.Brand href="#home">
-          <Image src={theme ? logo_l : logo_d} style={{ width: "30%", paddingRight: "10px" }} alt="logo" />
-          <p className='d-inline p-3 fs-3 '>RoboMed</p>
-          <button className='iconBtn' onClick={() => setTheme(s => !s)}>{theme ? <IoMoonOutline className='fs-5' /> : <IoSunnyOutline className='fs-5' />}</button>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav variant="pills" className="ms-auto" defaultActiveKey="#home">
-            <Nav.Item>
-              <Nav.Link href="#home">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#registrationForm">Registration Form</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#about">About</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#service">Service</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#faq">FAQ</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    return (
+        <Navbar collapseOnSelect expand="lg" variant={!theme ? "dark" : "light"}>
+            <Container className='me-auto'>
+                <Navbar.Brand href="#home">
+                    <Image src={theme ? logo_l : logo_d} style={{ width: "30%", paddingRight: "10px" }} alt="logo" />
+                    <p className='d-inline p-3 fs-3 '>RoboMed</p>
+                    <button className='iconBtn' onClick={() => setTheme(s => !s)}>{theme ? <IoMoonOutline className='fs-5' /> : <IoSunnyOutline className='fs-5' />}</button>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav variant="pills" className="ms-auto" defaultActiveKey="#home">
+                        <Nav.Item>
+                            <Nav.Link href="#home">Home</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#registrationForm">Registration Form</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#about">About</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#service">Service</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#faq">FAQ</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default NavbarComp;
